48fd5edfa56408c7943172fb22d38c3b2514b6c1
Samuele FAVAZZA <elesamu.89@gmail.com>
2016-09-11 18:23:44 +0200

    Fix: make class member directly accessible (no need of methods)

--- nsf_fix_util.py
+++ nsf_fix_util.py
@@ -6,19 +6,28 @@ class FixFmt:
                  signed: bool,
                  intBits,
                  fracBits):
-        self._signed = signed
-        self._intBits = intBits
-        self._fracBits = fracBits
+        self.signed = signed
+        self.intBits = intBits
+        self.fracBits = fracBits
 
     def __str__(self):
-        return "(" + str(self._signed) + "," \
-           + str(self._intBits) + "," \
-           + str(self._fracBits) + ")"
+        return "(" + str(self.signed) + "," \
+           + str(self.intBits) + "," \
+           + str(self.fracBits) + ")"
+
+    # def signed(self):
+    #     return self._signed
+
+    # def intBits(self):
+    #     return self._intBits
+
+    # def fracBits(self):
+    #     return self._fracBits
 
     def bit_length(self):
         return int(self._signed) \
-            + self._intBits \
-            + self._fracBits
+            + self.intBits \
+            + self.fracBits
 
     def range(self):
         """Return the range representable by fix format object"""
